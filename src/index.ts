c33a63aa8fba10d6b212bc05acbdde5162af6230
frostime <frostime@foxmail.com>
2024-01-24 12:55:11 +0800

    🐛 fix: 2.12.4 block icon incompatible

--- src/index.ts
+++ src/index.ts
@@ -145,11 +145,11 @@ export default class PluginSample extends Plugin {
                     dock.element.innerHTML = `<div class="fn__flex-1 fn__flex-column">
                     <div class="block__icons">
                         <div class="block__logo">
-                            <svg><use xlink:href="#iconEmoji"></use></svg>
+                            <svg class="block__logoicon"><use xlink:href="#iconEmoji"></use></svg>
                             Custom Dock
                         </div>
                         <span class="fn__flex-1 fn__space"></span>
-                        <span data-type="min" class="block__icon b3-tooltips b3-tooltips__sw" aria-label="Min ${adaptHotkey("⌘W")}"><svg><use xlink:href="#iconMin"></use></svg></span>
+                        <span data-type="min" class="block__icon b3-tooltips b3-tooltips__sw" aria-label="Min ${adaptHotkey("⌘W")}"><svg class="block__logoicon"><use xlink:href="#iconMin"></use></svg></span>
                     </div>
                     <div class="fn__flex-1 plugin-sample__custom-dock">
                         ${dock.data.text}
